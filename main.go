5603852626b29dbc743264ca7237faaced11c514
Harsha <harshavadluri.dev@gmail.com>
2025-05-25 00:45:54 +0530

    creating a user in postgres

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/iiharsha/rss-go/internal/database"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
 	godotenv.Load()
 
-	port := os.Getenv("PORT")
+	port := os.Getenv("PORT") //getting port from .env
 	if port == "" {
 		log.Fatal("PORT is missing idiot")
+	}
+
+	dbUrl := os.Getenv("DB_URL") //getting db_url from .env
+	if dbUrl == "" {
+		log.Fatal("DB_URL is missing idiot")
+	}
+
+	conn, err := sql.Open("postgres", dbUrl) //opening a db connection
+	if err != nil {
+		log.Fatal("can't connect to database", err)
+	}
+
+	//converting conn -> db.Queries
+	queries := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: queries,
 	}
 
 	router := chi.NewRouter()
@@ -33,6 +58,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
